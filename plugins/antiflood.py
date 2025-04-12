d67b97ad97fea7466a40ee25447bd720310272ec
Sosuke-Aizen-Git <shosuke.aizen99@gmail.com>
2025-04-13 04:47:36 +0530

    Update antiflood.py

--- plugins/antiflood.py
+++ plugins/antiflood.py
@@ -1,9 +1,9 @@
 import time
 import asyncio
 from collections import defaultdict
-from pyrofork import Client, filters
-from pyrofork.types import Message, ChatPermissions
-from pyrofork.errors import RPCError
+from pyrogram import Client, filters
+from pyrogram.types import Message, ChatPermissions
+from pyrogram.errors import RPCError
 from database import Chats
 
 # Default antiflood settings
